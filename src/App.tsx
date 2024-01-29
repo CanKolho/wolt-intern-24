a48868c5b2603d0cdc5918779a6a7dbecd9188b2
Can Kolho <can.kolho@aalto.fi>
2024-01-29 16:06:28 +0200

    Deleted the wolt image

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,8 @@
 import DeliveryFeeForm from "./components/DeliveryFeeform/DeliveryFeeForm";
-import logo from './assets/logo.png'
 
 const App = () => {
   return (
-    <>
-      <img src={logo} width={80} />
-      <DeliveryFeeForm />
-    </>
+    <DeliveryFeeForm />
   )
 }
 
