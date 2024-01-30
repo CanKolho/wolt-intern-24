96c5d14ce5a5f54e1a005ecbd225b83618f9fafe
Can Kolho <can.kolho@hotmail.com>
2024-01-31 00:14:42 +0200

    Deleted function that is not used

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,9 +1,3 @@
-export const validateDateInput = (input: string): boolean => {
-  // Regex to validate a date in the format DD-MM-YYYY
-  const regex = /^\d{2}-\d{2}-\d{4}$/;
-  return regex.test(input);
-}
-
 // Validate a number input with a decimal point
 export const validateInput = (input: string): boolean => {
     const regexPattern = /^\d+(\.\d+)?$/;
